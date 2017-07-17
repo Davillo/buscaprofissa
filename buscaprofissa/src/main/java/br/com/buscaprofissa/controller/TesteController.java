ebd4253915406e751e256ee91a7da769371b656c
Kass Maciel <Kass Maciel@DESKTOP-ON9464T.local>
2017-07-17 20:42:30 -0300

    tela cadastro inicial incompleta, alguns recursos do bootstrap

--- buscaprofissa/src/main/java/br/com/buscaprofissa/controller/TesteController.java
+++ buscaprofissa/src/main/java/br/com/buscaprofissa/controller/TesteController.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public final class TesteController {
 	
-	@RequestMapping("/teste")
-	public String teste(){
-		return "teste";
+	@RequestMapping("/cadastro")
+	public String login(){
+		return "cadastroInicial";
 	}
 	
+	@RequestMapping("/teste")
+	public String teste(){
+		return "login2";
+	}
 }
