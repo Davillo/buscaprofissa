307e616d5698a2e62a02f2ac50ccdc86b4cec1cd
Davillo <davilloaurelio@hotmail.com>
2017-09-11 21:36:13 -0300

    Classe para tratar usuario logado, configurações de páginação e ordenação

--- buscaprofissa/src/main/java/br/com/buscaprofissa/config/init/SecurityInitializer.java
+++ buscaprofissa/src/main/java/br/com/buscaprofissa/config/init/SecurityInitializer.java
@@ -1,7 +1,21 @@
 package br.com.buscaprofissa.config.init;
 
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 
+	@Override
+	protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+		FilterRegistration.Dynamic characterEncondingFilter = servletContext.addFilter("encondingFilter", 
+				new CharacterEncodingFilter());
+			
+		characterEncondingFilter.setInitParameter("encoding", "UTF-8");
+		characterEncondingFilter.setInitParameter("forceEncoding", "true");
+		characterEncondingFilter.addMappingForUrlPatterns(null, false, "/*");
+	}
+	
 }
---
+++ buscaprofissa/src/main/java/br/com/buscaprofissa/security/UsuarioLogado.java
@@ -0,0 +1,27 @@
+package br.com.buscaprofissa.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import br.com.buscaprofissa.model.Usuario;
+
+
+public class UsuarioLogado extends User {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Usuario usuario;
+
+	public UsuarioLogado(Usuario usuario, Collection<? extends GrantedAuthority> authorities) {
+		super(usuario.getEmail(), usuario.getSenha(), authorities);
+		this.usuario = usuario;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+}
